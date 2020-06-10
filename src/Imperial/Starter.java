e7e7d9724fb61a39b8e0e58963405d55c76b7a9e
Costi <costikhamis@gmail.com>
2020-06-10 12:28:27 -0700

    Re-implementation of verious classes
    Some classes are renamed to be more clear, TimeCardRow now extend ArrayList so they can contain many Day objects, ScheduleInterpreterA modified to not use first row, other classes modified to reflect these changes

--- src/Imperial/Starter.java
+++ src/Imperial/Starter.java
@@ -3,6 +3,7 @@ package Imperial;
 import java.util.ArrayList;
 
 import scheduleAnalyzer.ScheduleAnalyzer;
+import scheduleReader.ScheduleInterpreterA;
 
 public class Starter {
 	public static void main(String [] args) {
@@ -11,7 +12,7 @@ public class Starter {
 		paths.add("C:/users/costi/Onedrive/Desktop/Almaden Schedule 3.11 to 3.17.csv");
 		paths.add("C:/users/costi/Onedrive/Desktop/Almaden Schedule 3.18 to 3.24.csv");
 		paths.add("C:/users/costi/Onedrive/Desktop/Almaden Schedule 3.25 to 3.31.csv");
-		System.out.println("testingb");
-		ScheduleAnalyzer.intelligentMerge(paths);
+		System.out.println(ScheduleInterpreterA.convertToTimeArray("C:/users/costi/Onedrive/Desktop/Almaden Schedule 3.4 to 3.10.csv"));
+		//ScheduleAnalyzer.intelligentMerge(paths);
 	}
 }
--- src/scheduleAnalyzer/ScheduleAnalyzer.java
+++ src/scheduleAnalyzer/ScheduleAnalyzer.java
@@ -8,13 +8,13 @@ import scheduleReader.ScheduleInterpreterA;
 public class ScheduleAnalyzer {
 
 	public static void intelligentMerge(ArrayList<String> paths) {
-		TreeSet<WeeklyTimeCard> fullList = new TreeSet<WeeklyTimeCard>();
+		/**TreeSet<WeeklyTimeCard> fullList = new TreeSet<WeeklyTimeCard>();
 		for(String path : paths) {
 			WeeklyTimeCard timeCardSheet = ScheduleInterpreterA.convertToTimeArray(path);
 			fullList.add(timeCardSheet);
 
 			System.out.println(timeCardSheet.toString());
 			System.out.println();
-		}
+		}**/
 	}
 }
